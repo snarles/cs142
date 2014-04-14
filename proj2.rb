7ede02e19a75777e396fcde437509b4c6537d832
Charles Zheng <snarles@stanford.edu>
2014-04-14 20:37:00 +0000

    proj2

--- proj2.rb
+++ proj2.rb
@@ -1,12 +1,28 @@
 # squared.rb
 
 class Numeric
-  def squared
-    return self**2
-  end
+    def squared
+        return self**2
+    end
 end
 
 puts 9.squared
 puts 5.squared
 
 # sort.rb
+
+class String
+    def to_ff
+        temp=self[/\-?[0-9]\.?([0-9]*)/]
+        if temp==nil
+            return -1
+        end
+        return temp.to_f
+    end
+end
+
+class Array
+    def funny_sort
+        self.sort_by {|v| v.to_ff}
+    end
+end
