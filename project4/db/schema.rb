7b525d737ef26496a4324360c20a5efde95f52a4
Charles Zheng <snarles@stanford.edu>
2014-04-28 04:31:31 +0000

    add details to model photo

--- project4/db/schema.rb
+++ project4/db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140428032517) do
+ActiveRecord::Schema.define(version: 20140428042357) do
+
+  create_table "photos", force: true do |t|
+    t.string   "date_time"
+    t.string   "file_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "sessions", force: true do |t|
     t.string   "session_id", null: false
@@ -22,5 +29,12 @@ ActiveRecord::Schema.define(version: 20140428032517) do
 
   add_index "sessions", ["session_id"], name: "index_sessions_on_session_id", unique: true
   add_index "sessions", ["updated_at"], name: "index_sessions_on_updated_at"
+
+  create_table "users", force: true do |t|
+    t.string   "first_name"
+    t.string   "last_name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
 end
