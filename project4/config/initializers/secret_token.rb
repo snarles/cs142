abac0fa1050807bfbed7e76e5ed93513e0bc3e57
Charles Zheng <snarles@stanford.edu>
2014-04-28 03:19:39 +0000

    project 4 init

---
+++ project4/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Project4::Application.config.secret_key_base = 'a8ab463a4378b59ee0fe5712a9bb30fb4a6afff800c2fd2382467a486b4672deea86859007c75ae73a58f8361b53108b2be5dd8d30751bddb113ee156bec882d'
