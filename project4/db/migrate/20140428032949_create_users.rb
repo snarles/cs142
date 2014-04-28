3de0cdc8f7327f5d84491b70bf54db23cf988bd4
Charles Zheng <snarles@stanford.edu>
2014-04-28 04:21:11 +0000

    add details to model user

--- project4/db/migrate/20140428032949_create_users.rb
+++ project4/db/migrate/20140428032949_create_users.rb
@@ -1,7 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-
+      t.column :first_name, :string
+      t.column :last_name, :string
       t.timestamps
     end
   end
