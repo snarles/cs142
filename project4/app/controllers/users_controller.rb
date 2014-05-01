758b7ab4cb52b4d69054f396d6353a9becd7cc0a
Charles Zheng <snarles@stanford.edu>
2014-05-01 04:18:20 +0000

    user page

--- project4/app/controllers/users_controller.rb
+++ project4/app/controllers/users_controller.rb
@@ -3,4 +3,9 @@ class UsersController < ApplicationController
         @title = "List of Users"
         @users = User.all
     end
+    
+    def show
+        @user = User.find(params[:id])
+        @title = @user.first_name + " " + @user.last_name + " page"
+    end
 end
