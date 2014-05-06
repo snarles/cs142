696bf357fea9973bb9ba2d71c86d0be9ae42fbac
Charles Zheng <snarles@stanford.edu>
2014-05-06 05:45:19 +0000

    login and logout works

--- project5/app/controllers/users_controller.rb
+++ project5/app/controllers/users_controller.rb
@@ -14,6 +14,19 @@ class UsersController < ApplicationController
     end
     
     def post_login
-        session[:login] = params[:login]
+        @user = User.find_by_login(params[:login])
+        if @user.nil?
+            session[:login_error] = :not_found    
+            redirect_to :back, status: 302
+        else
+            session[:login] = params[:login]
+            session[:login_error] = nil    
+            redirect_to @user, status: 302
+        end
+    end
+    
+    def logout
+        session[:login] = nil
+        redirect_to action: :index, status: 302
     end
 end
--- project5/app/models/user.rb
+++ project5/app/models/user.rb
@@ -1,4 +1,5 @@
 class User < ActiveRecord::Base
     has_many :photos
     has_many :comments
+    validates :login, uniqueness: true
 end
